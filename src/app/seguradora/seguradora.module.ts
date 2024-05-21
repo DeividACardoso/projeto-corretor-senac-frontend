d882a27fc1fe22d41395095004f52e2b24b5430d
Vitor Garcia <vitorgarciadevasconcelos@gmail.com>
2024-05-20 21:55:31 -0300

    refactor: corrigido o phone pipe em seguradora

--- src/app/seguradora/seguradora.module.ts
+++ src/app/seguradora/seguradora.module.ts
@@ -5,14 +5,12 @@ import { SeguradoraRoutingModule } from './seguradora-routing.module';
 import { SeguradoraListagemComponent } from './seguradora-listagem/seguradora-listagem.component';
 import { SeguradoraDetalheComponent } from './seguradora-detalhe/seguradora-detalhe.component';
 import { FormsModule } from '@angular/forms';
-import { PhonePipe } from '../shared/phone-pipe/phone.pipe';
 
 
 @NgModule({
   declarations: [
     SeguradoraListagemComponent,
     SeguradoraDetalheComponent,
-    PhonePipe
   ],
   imports: [
     CommonModule,
