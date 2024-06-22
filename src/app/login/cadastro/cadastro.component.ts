2e42b3f8aaabc7d08ef3e01bb65a5e6ae932bac2
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2024-06-22 12:14:22 -0300

    refactor: Implementado a verificação de confirmação de senha no formulário de registro

--- src/app/login/cadastro/cadastro.component.ts
+++ src/app/login/cadastro/cadastro.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RegisterDTO } from '../../shared/model/register.dto';
+import { CorretorService } from '../../shared/service/corretor.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,12 +11,47 @@ import { RegisterDTO } from '../../shared/model/register.dto';
 })
 export class CadastroComponent {
 
+  constructor(private corretorService: CorretorService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
   public dto: RegisterDTO = new RegisterDTO();
   public confirmarSenha: string;
 
 
-  cadastrar() {
-    //TODO: Implementar a chamada para o serviço de cadastro
+  public cadastrar() {
+    try {
+      if (this.dto.senha !== this.confirmarSenha) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'As senhas não coincidem!'
+        });
+      } else {
+        this.corretorService.register(this.dto).subscribe(
+          (data: any) => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Sucesso!',
+              text: 'Cadastro realizado com sucesso! Você será redirecionado para a tela de login.'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                this.router.navigate(['/login']);
+              }
+            });
+          },
+          (error: any) => {
+            console.error(error);
+          }
+        );
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
   }
 
+  voltar() {
+    this.router.navigate(['/login']);
+  }
 }
