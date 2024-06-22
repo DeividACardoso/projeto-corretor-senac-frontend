26378951b2b690eb21a281d9e869c04aea1edf7a
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2024-06-22 12:14:45 -0300

    refactor: Atualização do componente da barra de navegação para incluir a funcionalidade de logout
    O componente da barra de navegação foi atualizado para incluir uma funcionalidade de logout. Quando o link "Sair" é clicado, uma caixa de diálogo de confirmação é exibida para o usuário. Se o usuário confirmar, o token será removido do armazenamento local e o usuário será redirecionado para a página de login.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  constructor(private router: Router) { }
+
+  public logout() {
+    Swal.fire({
+      title: 'Você tem certeza?',
+      text: 'Deseja sair do sistema?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, sair',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem('token');
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
