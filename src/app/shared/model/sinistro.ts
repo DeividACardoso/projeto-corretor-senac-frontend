6eb2306465b5b8154cc9f86dc4a7cf731c46e6a9
Vitor Garcia0 <vitorgarciadevasconcelos@gmail.com>
2024-06-24 11:05:04 -0300

    refactor: mudanndo os valores de Sinistro model

--- src/app/shared/model/sinistro.ts
+++ src/app/shared/model/sinistro.ts
@@ -1,6 +1,13 @@
+import { Time } from "@angular/common";
+import { Seguro } from "./seguro";
+
 export class Sinistro {
   id: number;
   tipo: string;
+  // data: Date
+  data: Date;
+  horario: Time;
   dataHora: Date;
   descricao: string;
+  seguro: Seguro;
 }
