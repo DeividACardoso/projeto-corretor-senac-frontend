7adbe7a6d67e86659a1b9d5bff22b8cf80f1b60e
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2024-06-25 18:54:35 -0300

    feat: Login é bloqueado por 1 minuto após 5 tentativas erradas e por 10 minutos após 7 tentativas erradas.

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -4,6 +4,7 @@ import { AuthenticationDTO } from '../../shared/model/authentication.dto';
 import { CommonModule } from '@angular/common';
 import { CorretorService } from '../../shared/service/corretor.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -14,29 +15,80 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class LoginComponent {
 
   public dto: AuthenticationDTO = new AuthenticationDTO();
+  public form: NgForm;
 
   constructor(private corretorService: CorretorService,
     private router: Router,
     private route: ActivatedRoute
   ) { }
 
+
   public login() {
-    console.log('Fazendo login: ', this.dto);
+    localStorage.removeItem('token');
     this.corretorService.login(this.dto).subscribe(
       (resultado: any) => {
         console.log('Login realizado com sucesso: ', resultado.token);
+        this.wrongAttempts = 0;
         this.corretorService.storeToken(resultado.token);
         this.router.navigate(['/clientes/lista'])
-
       },
       (erro: any) => {
-        console.log('Erro ao fazer login: ', erro);
+        if(this.dto.login == '' || this.dto.password == ''){
+          alert('Preencha todos os campos!');
+        }
+        if(erro.status == 403 || erro.status == 400){
+          this.wrongAttempts++;
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Usuário ou senha inválidos!',
+          })
+        }
       }
     )
   }
-  
+
   public cadastro() {
     this.router.navigate(['login/cadastro'])
   }
+
+  private wrongAttempts: number = 0;
+
+  public loginWithTimeout() {
+    if (this.wrongAttempts >= 5) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Você excedeu o número de tentativas de login permitidas. Tente novamente em 1 minuto.',
+      });
+      setTimeout(() => {
+        this.wrongAttempts = 0;
+      }, 60000);
+      return;
+    }
+
+    if (this.wrongAttempts >= 7) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Você excedeu o número de tentativas de login permitidas. Tente novamente em 10 minutos.',
+      });
+      setTimeout(() => {
+        this.wrongAttempts = 0;
+      }, 600000);
+      return;
+    }
+
+    this.login();
+  }
+
+  ngOnInit() {
+    this.wrongAttempts = Number(localStorage.getItem('wrongAttempts')) || 0;
+  }
+
+  ngOnDestroy() {
+    localStorage.setItem('wrongAttempts', String(this.wrongAttempts));
+  }
+
 }
-  
+
